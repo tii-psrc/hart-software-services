192d4b6b3c5e47d14bfc7d7354fed198657ea647
AlexeyM <ledeus@gmail.com>
2025-09-18 13:13:53 +0300

    Scai flash dbg: set byte mode for data

--- baremetal/drivers/scai_fpga/scai_fpga_common.c
+++ baremetal/drivers/scai_fpga/scai_fpga_common.c
@@ -65,12 +65,13 @@ void QSPI_FPGA_IF_transfer(uintptr_t base_addr, const uint8_t* tx_buffer, uint32
     
     volatile uint32_t* data_reg = (uint32_t*)(base_addr + QSPI_DATA_REG_OFFSET);
     volatile uint32_t* ctrl1_reg = (uint32_t*)(base_addr + QSPI_CTRL1_REG_OFFSET);
-mHSS_DEBUG_PRINTF(LOG_NORMAL, "Address in ctrl1_reg -> %p\n", (void*)ctrl1_reg);
-mHSS_DEBUG_PRINTF(LOG_NORMAL, "Address in data_reg -> %p\n", (void*)data_reg);
 
     // Construct CTRL1
     uint32_t ctrl1_val = 0;
-    ctrl1_val |= Q_CTRL1_SET_nRESET;
+    ctrl1_val |= Q_CTRL1_SET_nRESET; // clear reset
+
+    // choose BYTE mode
+    ctrl1_val |= QSPI_CTRL1_BYTE_MODE; 
 
     if (format == MSS_QSPI_QUAD_FULL) {
         ctrl1_val |= QSPI_CTRL1_QUAD_MODE;
@@ -88,24 +89,22 @@ mHSS_DEBUG_PRINTF(LOG_NORMAL, "Address in data_reg -> %p\n", (void*)data_reg);
 
     // Write FIFO
     for (uint32_t i = 0; i < tx_len; ++i) {
-        mHSS_DEBUG_PRINTF(LOG_NORMAL, "DATA_T = 0x%08X\n", ((uint32_t)tx_buffer[i]) << 24);
-        *data_reg = ((uint32_t)tx_buffer[i]) << 24;
+        mHSS_DEBUG_PRINTF(LOG_NORMAL, "DATA_T = 0x%08X\n", tx_buffer[i]);
+        *data_reg = tx_buffer[i]; 
     }
 
-    bool idle_flag = QSPI_FPGA_IF_wait_controller_idle(base_addr);
-    mHSS_DEBUG_PRINTF(LOG_NORMAL, "idle = %d\n", idle_flag);
+    QSPI_FPGA_IF_wait_controller_idle(base_addr);
 
     // Read FIFO
     for (uint32_t i = 0; i < rx_len; ++i) {
         // rx_buffer[i] = (uint8_t)(*data_reg);
         uint32_t value = *data_reg;
         mHSS_DEBUG_PRINTF(LOG_NORMAL, "DATA_R = 0x%08X\n", value);
-        rx_buffer[i] = (uint8_t)(value >> 24);
+        rx_buffer[i] = (uint8_t)(value);
     }
 
-    // Clen after operation
-    uint32_t final_ctrl_val = Q_CTRL1_SET_nRESET; // Начинаем с базового состояния
-
+    // --- Очистка после операции ---
+    uint32_t final_ctrl_val = Q_CTRL1_SET_nRESET;
     if (keep_ce_active) {
         final_ctrl_val |= QSPI_CTRL1_CE_ACTIVATE;
     }

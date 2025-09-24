6d9fda9e1f322ec9248fa865e7fc1ff4498c8212
AlexeyM <ledeus@gmail.com>
2025-09-24 15:54:12 +0300

    Printing status reg

--- baremetal/drivers/scai_fpga/micron_mt29f_fpga.c
+++ baremetal/drivers/scai_fpga/micron_mt29f_fpga.c
@@ -88,6 +88,13 @@ static const uint8_t  MT29F_UNLOCK_ALL   = 0x00;                                
 
 // STatus register bits
 static const uint8_t MT29F_STATUS_OIP_B  = 0x01;
+static const uint8_t MT29F_STATUS_WEL    = 0x02;
+static const uint8_t MT29F_STATUS_E_FAIL = 0x04;
+static const uint8_t MT29F_STATUS_P_FAIL = 0x08;
+static const uint8_t MT29F_STATUS_ECC_S0 = 0x10;
+static const uint8_t MT29F_STATUS_ECC_S1 = 0x20;
+static const uint8_t MT29F_STATUS_ECC_S2 = 0x40;
+static const uint8_t MT29F_STATUS_CRBSY  = 0x80;
 
 // --- Static Helper Functions ---
 
@@ -367,12 +374,18 @@ uint8_t SCAI_MT29_Flash_erase_block(scai_fpga_channel_t* channel, uint16_t block
     scai_fpga_transaction(channel, &params);
 
     mHSS_DEBUG_PRINTF(LOG_ERROR, "MT29_Flash_erase_block: wait\n");
+    uint8_t stat_reg = get_feature(channel, MT29F_REG_STATUS);
+    mHSS_DEBUG_PRINTF(LOG_ERROR, "MT29_Flash_erase_block: stat_reg = %u\n", stat_reg);
+
     uint8_t status = wait_flash_ready(channel);
     if (status != 0) {
         mHSS_DEBUG_PRINTF(LOG_ERROR, "MT29_Flash_erase_block: wait timeout\n");
         return 1;
     }
 
+    uint8_t stat_reg = get_feature(channel, MT29F_REG_STATUS);
+    mHSS_DEBUG_PRINTF(LOG_ERROR, "MT29_Flash_erase_block: stat_reg = %u\n", stat_reg);
+    
     return 0;
 }
 

e14e8c43ea7545d5e1b915e0f29b608d6bf68e7d
AlexeyM <alexey.mineev@tii.ae>
2025-10-31 12:42:12 +0200

    Removed debug output

--- baremetal/drivers/scai_fpga/micron_mt29f_fpga.c
+++ baremetal/drivers/scai_fpga/micron_mt29f_fpga.c
@@ -267,12 +267,9 @@ void SCAI_MT29_Flash_init(scai_fpga_channel_t* channel, mss_qspi_io_format io_fo
     mt29f_config_reg_t config_reg;
     config_reg.byte = get_feature(channel, MT29F_REG_CONFIG);
 
-    mHSS_DEBUG_PRINTF(LOG_NORMAL, "get_feature %02X = %02X\n", MT29F_REG_CONFIG, config_reg.byte);
-
     if (!config_reg.bits.conti_rd) {
         config_reg.bits.conti_rd = 1;
         set_feature(channel, MT29F_REG_CONFIG, config_reg.byte);
-        mHSS_DEBUG_PRINTF(LOG_NORMAL, "set_feature %02X = %02X\n", MT29F_REG_CONFIG, config_reg.byte);
     }
 
     unlock_all_blocks(channel);    

0c8bd2f05d3e7e499cd84fc8d224dd590bb107d1
kwangsu-unikie <kwangsu.jung@unikie.com>
2026-08-10 08:27:09 +0000

    scai_telemetry_remoteproc: stop external wdog added

--- services/wdog/wdog_external.h
+++ services/wdog/wdog_external.h
@@ -10,6 +10,9 @@ void wdog_external_init(void);
 void wdog_external_idle(void);
 void wdog_external_monitoring(void);
 
+int wdog_external_stop(void);
+int wdog_external_status(void);
+
 #ifdef __cplusplus
 }
 #endif

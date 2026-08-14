c9a4b2788c20ea6d8690bc853c86ff854dc1689b
kwangsu-unikie <kwangsu.jung@unikie.com>
2026-08-14 09:32:30 +0000

    telemetry publish: disable publishing when Linux boot completed due to unknown error.

--- services/opensbi/opensbi_telemetry_ecall.c
+++ services/opensbi/opensbi_telemetry_ecall.c
@@ -119,7 +119,7 @@ static int __stop_services(uint32_t service_id)
 		case SBI_TM_EXT_STOP_PUBLISHING:
 #if IS_ENABLED(CONFIG_SERVICE_TELEMETRY_PUBLISH)
 			format_log(HSS_HART_E51, buf, "[%s] args(%d)\r\n", __func__, service_id);
-			do_tm_publish(HSS_BOOT_BS_LINUX_BOOT_SUCEEDED);
+			//do_tm_publish(HSS_BOOT_BS_LINUX_BOOT_SUCEEDED);
 			if (tm_set_stop_publish()) {
 				result = SBI_OK;
 			}
